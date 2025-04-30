85b8bd3ddfd6815e49776a452a4e72ad4c4302a4
Gurmukhnishan Singh <gsingh@quilr.ai>
2025-04-30 22:14:38 +0530

    added new check

---
+++ pre_commit_hooks/check_attribute.py
@@ -0,0 +1,23 @@
+import argparse
+from collections.abc import Generator
+from collections.abc import Sequence
+from typing import Any
+from typing import NamedTuple
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-m', '--multi', '--allow-multiple-documents', action='store_true',
+    )
+    parser.add_argument(
+        '--unsafe', action='store_true',
+        help=(
+            'Instead of loading the files, simply parse them for syntax.  '
+            'A syntax-only check enables extensions and unsafe constructs '
+            'which would otherwise be forbidden.  Using this option removes '
+            'all guarantees of portability to other yaml implementations.  '
+            'Implies --allow-multiple-documents'
+        ),
+    )
+    print(parser)
