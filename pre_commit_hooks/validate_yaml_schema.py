26127ed2320c7d7315d2ede7b5249e559fe8ca0a
gurjeet-quilr <gurjeetsingh@quilr.ai>
2025-05-09 17:41:36 +0530

    testing

--- pre_commit_hooks/validate_yaml_schema.py
+++ pre_commit_hooks/validate_yaml_schema.py
@@ -105,7 +105,7 @@ deploy_agent_schema = {
                 "output_instructions": { "type": "string" },
                 "communication": { "type": "string" },
                 "guardrails": {
-                  "type": { "string","array" }
+                  "type": ["string", "array"]
                 }
               },
               "required": [
